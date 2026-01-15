44ade0da49500a2b540594a4a8ef710d56cd590d
Daison <daisdaniel05@gmail.com>
2026-01-15 10:38:16 +0530

    Fix Android build by reverting MainActivity to default

--- safe-q-app/android/app/src/main/java/com/safeq/app/MainActivity.java
+++ safe-q-app/android/app/src/main/java/com/safeq/app/MainActivity.java
@@ -1,23 +1,5 @@
 package com.safeq.app;
 
-import android.os.Bundle;
-
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.Plugin;
-import com.safeq.app.plugins.wifiaware.WifiAwarePlugin;
 
-import java.util.ArrayList;
-
-public class MainActivity extends BridgeActivity {
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-
-    // Initializes the Bridge
-    this.init(savedInstanceState, new ArrayList<Class<? extends Plugin>>() {{
-      // Additional plugins you've installed go here
-      // Ex: add(TotallyAwesomePlugin.class);
-      add(WifiAwarePlugin.class);
-    }});
-  }
-}
+public class MainActivity extends BridgeActivity {}
